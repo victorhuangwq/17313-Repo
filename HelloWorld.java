ad658c6c515e47d1dc7e6dcfc30b730c44108205
Victor Huang <victorhuangwq@gmail.com>
2020-09-05 08:47:10 +0800

    add years at CMU

--- HelloWorld.java
+++ HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World, I have been at CMU for 1 year");
     }
 
 }
